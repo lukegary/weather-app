e6d05dd99efb35b89fcd4323c49da5f3265a5838
Luke Gary <lukegary@gmail.com>
2019-06-15 14:45:58 -0400

    add store for pressure humidity and temp

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LocationForm from './LocationForm'
+import LocationForm from './LocationForm';
+import MyChart from './MyChart';
 
 const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
 
 export default class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    humidity: undefined,
+    pressure: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
     const latitude = e.target.elements.latitude.value;
     const longitude = e.target.elements.longitude.value;
     const city = e.target.elements.city.value;
-
+    let request = "";
     if(latitude && longitude) {
-      const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Api_Key}`);
-      const response = await request.json();
-      console.log(response);
+      request = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${Api_Key}`);
     } else if(city) {
-      const request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${Api_Key}`);
-      const response = await request.json();
-      console.log(response);
+      request = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${Api_Key}`);
     }
-
+    const response = await request.json();
+    this.setState({
+      temperature: response.main.temp,
+      city: response.name,
+      humidity: response.main.humidity,
+      pressure: response.main.pressure
+    })
   }
   render() { 
     return (
       <div className="App">
           <LocationForm loadWeatherData={this.getWeather} />
+          <MyChart />
       </div>
     );
   }
